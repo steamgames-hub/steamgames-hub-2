8347793a3107d8eede2a868e002fd7a646af48e3
David Romero <drorganvidez@us.es>
2025-09-18 12:56:41 +0200

    fix: Fixes linter worflow

--- rosemary/commands/selenium.py
+++ rosemary/commands/selenium.py
@@ -1,14 +1,15 @@
 import os
 import subprocess
+
 import click
 
 
-@click.command('selenium', help="Executes Selenium tests based on the environment.")
-@click.argument('module', required=False)
+@click.command("selenium", help="Executes Selenium tests based on the environment.")
+@click.argument("module", required=False)
 def selenium(module):
     # Absolute paths
-    working_dir = os.getenv('WORKING_DIR', '')
-    modules_dir = os.path.join(working_dir, 'app/modules')
+    working_dir = os.getenv("WORKING_DIR", "")
+    modules_dir = os.path.join(working_dir, "app/modules")
 
     def validate_module(module):
         """Check if the module exists."""
@@ -16,11 +17,10 @@ def selenium(module):
             module_path = os.path.join(modules_dir, module)
             if not os.path.exists(module_path):
                 raise click.UsageError(f"Module '{module}' does not exist.")
-            selenium_test_path = os.path.join(module_path, 'tests', 'test_selenium.py')
+            selenium_test_path = os.path.join(module_path, "tests", "test_selenium.py")
             if not os.path.exists(selenium_test_path):
                 raise click.UsageError(
-                    f"Selenium test for module '{module}' does not exist at path "
-                    f"'{selenium_test_path}'."
+                    f"Selenium test for module '{module}' does not exist at path " f"'{selenium_test_path}'."
                 )
 
     def run_selenium_tests_in_local(module):
@@ -46,29 +46,32 @@ def selenium(module):
         click.echo(f"Running Selenium tests with command: {' '.join(test_command)}")
         subprocess.run(test_command, check=True)
 
-
     # Validate module if provided
     if module:
         validate_module(module)
 
-    if working_dir == '/app/':
+    if working_dir == "/app/":
 
-        click.echo(click.style(
-            "Currently it is not possible to run this "
-            "command from a Docker environment, do you want to implement it yourself? ^^",
-            fg='red'
-        ))
+        click.echo(
+            click.style(
+                "Currently it is not possible to run this "
+                "command from a Docker environment, do you want to implement it yourself? ^^",
+                fg="red",
+            )
+        )
 
-    elif working_dir == '':
+    elif working_dir == "":
         run_selenium_tests_in_local(module)
 
-    elif working_dir == '/vagrant/':
+    elif working_dir == "/vagrant/":
 
-        click.echo(click.style(
-            "Currently it is not possible to run this "
-            "command from a Vagrant environment, do you want to implement it yourself? ^^",
-            fg='red'
-        ))
+        click.echo(
+            click.style(
+                "Currently it is not possible to run this "
+                "command from a Vagrant environment, do you want to implement it yourself? ^^",
+                fg="red",
+            )
+        )
 
     else:
-        click.echo(click.style(f"Unrecognized WORKING_DIR: {working_dir}", fg='red'))
+        click.echo(click.style(f"Unrecognized WORKING_DIR: {working_dir}", fg="red"))
