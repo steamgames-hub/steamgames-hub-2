72b18887a55a070a05e6f7f2fba490905b8749ec
Julia Angeles Burgos <julangbur@alum.us.es>
2025-11-10 01:23:39 +0100

    feat: Añadir campo download_count a Hubfile Closes: #1

--- app/modules/hubfile/models.py
+++ app/modules/hubfile/models.py
@@ -13,6 +13,7 @@ class Hubfile(db.Model):
     name = db.Column(db.String(120), nullable=False)
     checksum = db.Column(db.String(120), nullable=False)
     size = db.Column(db.Integer, nullable=False)
+    download_count = db.Column(db.Integer, nullable=False, default=0)
     feature_model_id = db.Column(db.Integer, db.ForeignKey("feature_model.id"), nullable=False)
 
     def get_formatted_size(self):
